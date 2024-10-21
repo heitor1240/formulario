6581ad307c979e8b116d5b1c4c24583ec2d8b138
heitor1240 <111541276+heitor1240@users.noreply.github.com>
2024-10-20 22:38:50 -0300

    push

---
+++ script.js
@@ -0,0 +1,17 @@
+document.getElementById('cadastroForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    const nome = document.getElementById('nome').value;
+    const email = document.getElementById('email').value;
+
+    const tabela = document.getElementById('tabelaCadastro').getElementsByTagName('tbody')[0];
+    const novaLinha = tabela.insertRow();
+
+    const celulaNome = novaLinha.insertCell(0);
+    const celulaEmail = novaLinha.insertCell(1);
+
+    celulaNome.textContent = nome;
+    celulaEmail.textContent = email;
+
+    document.getElementById('cadastro').reset();
+});
